e4831dd8b074e0f129e76b2c9be5ca3a7d21c58d
maroua hba <marouahba31@gmail.com>
2024-07-04 06:38:13 +0100

    Add basic routing setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
+import AppRouter from "@routes/AppRouter";
+// styles
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@styles/global.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <></>
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(<AppRouter />);
